b8d23aa19b2f3a80469fa55f9367f36d45eee814
root <daniel.ntaye@sahanalytics.com>
2024-11-26 20:05:15 +0000

    update producteur form

--- lib/app/modules/producteurForm/views/producteur_form_view.dart
+++ lib/app/modules/producteurForm/views/producteur_form_view.dart
@@ -16,49 +16,75 @@ class ProducteurFormView extends GetView<ProducteurFormController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Vx.white,
       appBar: AppBar(
-        title: const Text('ProducteurFormView'),
+        title: const Text('Ajouter un producteur'),
         centerTitle: true,
       ),
       body: Form(
         key: controller.formKey,
         child: VStack([
-          TextFormField(
-            controller: controller.nom_controller,
-            decoration: this.getTFStyle("Nom"),
-          ).marginOnly(bottom: 20),
-
-          TextFormField(
-            controller: controller.nom_controller,
-            decoration: this.getTFStyle("Prénoms"),
-          ).marginOnly(bottom: 20),
-
-          VStack([
-            "Genre".text.make(),
-            10.heightBox,
-            HStack([
-              HStack([
-                Radio(value: 'Homme', groupValue: 'sexe', onChanged: (value)=>{
-                  controller.sexe_controller(value.toString())
-                }),
-                'Homme'.text.make()
-              ]).expand(),
-              HStack([
-                Radio(value: 'Femme', groupValue: 'sexe', onChanged: (value)=>{
-                  controller.sexe_controller(value.toString())
-                }),
-                'Femme'.text.make()
-              ]).expand()
-            ]).marginOnly(bottom: 20),
-          ]),
+          
+          HStack([
+            SizedBox(
+              width: Get.width / 2.3,
+              child: TextFormField(
+                controller: controller.nom_controller,
+                decoration: this.getTFStyle("Nom"),
+              ),
+            ),
+            Spacer(),
+            SizedBox(
+              width: Get.width / 2.3,
+              child: TextFormField(
+                controller: controller.nom_controller,
+                decoration: this.getTFStyle("Prénoms"),
+              ),
+            ),
+            
+          ], alignment: MainAxisAlignment.spaceBetween,).marginOnly(bottom: 20),
+          
 
           TextFormField(
             keyboardType: TextInputType.numberWithOptions(),
             controller: controller.telephone_controller,
             decoration: this.getTFStyle("Numéro de téléphone"),
           ).marginOnly(bottom: 20),
+
+          VStack([
+            "Genre".text.make(),
+            10.heightBox,
+            HStack([
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Vx.gray500)
+                ),
+               child: HStack([
+                Radio(value: 'Homme', groupValue: 'sexe', onChanged: (value)=>{
+                  controller.sexe_controller(value.toString())
+                }),
+                'Homme'.text.make()
+              ])).expand(),
+              
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Vx.gray500)
+                ),
+               child: HStack([
+                Radio(value: 'Homme', groupValue: 'sexe', onChanged: (value)=>{
+                  controller.sexe_controller(value.toString())
+                }),
+                'Homme'.text.make()
+              ])).expand()
+            ], spacing: 12,).marginOnly(bottom: 20),
+          ]),
+          
+          
           
           InputDatePickerFormField(
+            
             onDateSaved: (DateTime date) => controller.date_naissance_controller.text = date.toIso8601String(),
             firstDate: DateTime.now().subtract(Duration(days: 160 * 365)),
             lastDate: DateTime.now().add(Duration(days: 160 * 365)),
@@ -73,10 +99,14 @@ class ProducteurFormView extends GetView<ProducteurFormController> {
           VStack([
             "Coopérative".text.make(),
             10.heightBox,
-            DropdownButtonFormField(
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all()
+              ),
+              child: DropdownButtonFormField(
                   isExpanded: false,
                   padding: const EdgeInsets.symmetric(horizontal: 5),
-                  borderRadius: BorderRadius.circular(5),
+                  borderRadius: BorderRadius.circular(0),
                   decoration: const InputDecoration(
                       fillColor: Color.fromRGBO(243, 243, 243, 1),
                       border: InputBorder.none
@@ -94,19 +124,32 @@ class ProducteurFormView extends GetView<ProducteurFormController> {
                   },
 
                 ),
+            ),
           ]),
-          Spacer(),
-          30.heightBox,
-          GFButton(
-            size: GFSize.LARGE,
-            onPressed: () {
-              
-            },
-            blockButton: true,
-            color: Vx.green700,
-            child: "Enrégistrer".text.make().pSymmetric(h: 3),
-          )
-        ]).p(25),
+          
+          60.heightBox,
+          HStack([
+            GFButton(
+              borderShape: ContinuousRectangleBorder(),
+              borderSide: BorderSide(width: 2.0, color: Theme.of(context).primaryColor),
+              size: GFSize.LARGE,
+              onPressed: () {
+                
+              },
+              color: Vx.white,
+              child: "Enrégistrer".text.color(Theme.of(context).primaryColor).make().pSymmetric(h: 3),
+            ).expand(),
+            GFButton(
+              borderShape: ContinuousRectangleBorder(),
+              size: GFSize.LARGE,
+              onPressed: () {
+                
+              },
+              color: Theme.of(context).primaryColor,
+              child: "Enrégistrer".text.make().pSymmetric(h: 3),
+            ).expand(),
+          ], spacing: 5,)
+        ]).scrollVertical().p(25),
       ),
     );
   }
@@ -115,7 +158,7 @@ class ProducteurFormView extends GetView<ProducteurFormController> {
   InputDecoration getTFStyle(String label, {bool? isObscure = false}){
     return InputDecoration(
       label: "${label}".text.make(),
-      border: OutlineInputBorder()
+      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.zero))
     );
   }
 }
